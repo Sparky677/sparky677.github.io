d821d64690b7d61d98a72df49aadc95e99c642a3
Caleb Holland <cholland8602@gmail.com>
2021-04-24 18:21:07 -0700

    hopefully fixed

--- scripts/indexScripts.js
+++ scripts/indexScripts.js
@@ -1,4 +1,7 @@
-let date = document.lastModified;
+let lastUpdate = document.lastModified;
+var d = new Date();
+var n = d.getFullYear();
 function lastUpdated() {
-  document.getElementById("updated").innerHTML="Last Updated: " + date;
+  document.getElementById("updated").innerHTML="Last Updated: " + lastUpdate;
+  document.getElementById("test").innerHTML= n + " | Caleb D. Holland | Washington | ";
 }
