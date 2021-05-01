d98bd87e4ee2def1cb4a2eff26fe34d6968038e6
Caleb Holland <cholland8602@gmail.com>
2021-04-30 22:16:32 -0700

    ranch finished

--- scripts/indexScripts.js
+++ scripts/indexScripts.js
@@ -3,5 +3,5 @@ var d = new Date();
 var n = d.getFullYear();
 function lastUpdated() {
   document.getElementById("updated").innerHTML="Last Updated: " + lastUpdate;
-  document.getElementById("test").innerHTML= n + " | Caleb D. Holland | Washington | ";
+  document.getElementById("test").innerHTML= "©" + n + " | Caleb D. Holland | Washington | ";
 }
