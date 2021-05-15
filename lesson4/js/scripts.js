795560556a9c10238bc5cc38b1d9ea53d1fbf832
Caleb Holland <cholland8602@gmail.com>
2021-05-14 18:39:39 -0700

    general updates

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -3,7 +3,7 @@ var d = new Date();
 var n = d.getFullYear();
 function lastUpdated() {
   document.getElementById("updated").innerHTML="Last Updated: " + lastUpdate;
-  document.getElementById("copy").innerHTML= "©" + n + " | Caleb D. Holland | Washington | ";
+  document.getElementById("copy").innerHTML= "©" + n + " | Caleb D. Holland |";
 }
 function callFuncts() {
   lastUpdated();
