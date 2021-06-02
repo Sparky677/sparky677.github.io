fbd29de2697f597185e518326b867fb09d63061a
Caleb Holland <cholland8602@gmail.com>
2021-06-02 16:58:00 -0700

    lesson 7 created

---
+++ lesson7/js/scripts.js
@@ -0,0 +1,33 @@
+WebFont.load({
+  google: {
+    families: [
+       'Quicksand', 'sans-serif'
+    ]
+  }
+});
+
+let lastUpdate = document.lastModified;
+var d = new Date();
+var n = d.getFullYear();
+function lastUpdated() {
+  document.getElementById("updated").innerHTML="Last Updated: " + lastUpdate;
+  document.getElementById("copy").innerHTML= "©" + n + " | Caleb D. Holland";
+}
+
+function fribanner() {
+  var day=new Date();
+  var now=day.getDay();
+  if (now==5) {
+    document.getElementById("date").classList="today";
+  }
+}
+
+function callFuncts() {
+  lastUpdated();
+  fribanner();
+  WindChill();
+}
+
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
