5e983ae26da2f996911de54222764ee52de30e7a
Caleb Holland <cholland8602@gmail.com>
2021-06-03 18:24:56 -0700

    intersection observer done

--- lesson7/js/scripts.js
+++ lesson7/js/scripts.js
@@ -1,31 +1,56 @@
-WebFont.load({
-  google: {
-    families: [
-       'Quicksand', 'sans-serif'
-    ]
-  }
-});
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
 
-let lastUpdate = document.lastModified;
-var d = new Date();
-var n = d.getFullYear();
+const options = {
+  rootMargin: '0px 0px -200px 0px',
+  threshold: 0
+};
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items) => {
+      items.forEach((item)=> {
+          if (item.isIntersecting) {
+              loadImages(item.target);
+              observer.unobserve(item.target);
+          }
+      });
+  }, options);
+
+  imagesToLoad.forEach((img) => {
+      observer.observe(img);
+  });
+
+} else {
+  imagesToLoad.forEach(img => {
+      loadImages(img);
+  });
+}
+
+
+
 function lastUpdated() {
+  let lastUpdate = document.lastModified;
+  var d = new Date();
+  var n = d.getFullYear();
   document.getElementById("updated").innerHTML="Last Updated: " + lastUpdate;
   document.getElementById("copy").innerHTML= "©" + n + " | Caleb D. Holland";
 }
 
-function fribanner() {
-  var day=new Date();
-  var now=day.getDay();
-  if (now==5) {
-    document.getElementById("date").classList="today";
-  }
-}
 
 function callFuncts() {
   lastUpdated();
-  fribanner();
-  WindChill();
+  WebFont.load({
+    google: {
+      families: [
+         'Quicksand', 'sans-serif'
+      ]
+    }
+  });
 }
 
 function toggleMenu() {
